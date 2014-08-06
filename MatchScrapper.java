bfa1a2cc0cc95a00872e372f3ed655ce29431800
Jeremiah Martinez <jeremiah@cs.utexas.edu>
2014-08-05 23:24:32 -0500

    working on MatchScrapper. Got it to arrive to the statistics page.

--- MatchScrapper.java
+++ MatchScrapper.java
@@ -1,9 +1,12 @@
 package SeleniumScrapper;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,14 +19,38 @@ public class MatchScrapper {
         
         // gotta get a list of the countries, but like with CountryStatScrapper,
         // have to copy the urls and names before we advance with the driver.
-      
+        Integer date;
         int startDate = 20140612;
-        List<WebElement> containerOfAllMatches = driver.findElements(By.xpath("//div[@class='matches']//a[@class='mu-m-ink']"));
-        for (int i = startDate; i < 20140713; i++) {
+        String countryURL;
+        List<WebElement> countryLinks;
+        ArrayList<String> containerOfAllMatches = new ArrayList<String>();
+        for (int i = startDate; i < 20140714; i++) {
+        	
+        	try {
+	        	date = new Integer(i);
+	        	countryLinks = driver.findElements(By.xpath(
+	        			"//div[@class='matches']//div[@id='" + date.toString() + "']//a"));
+	        	for (int k = 0; k < countryLinks.size(); k++) {
+	        		countryURL = countryLinks.get(k).getAttribute("href");
+	            	containerOfAllMatches.add(countryURL);
+	        	}
+        	} catch (NoSuchElementException e) {
+        		
+        	}
         	
         }
-        // get link to the statistics
+        System.out.printf("%d\n", containerOfAllMatches.size());
         
-        // arrive to statistics page.
+//        WebElement page;
+        for (int i = 0; i < containerOfAllMatches.size(); i++) {
+        	driver.get(containerOfAllMatches.get(i));
+        	driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        	
+        	WebElement page = driver.findElement(// By.id("mitem-statistics"));
+        			By.xpath("//div[@class='nav-wrap']//li[@id='mitem-statistics']//a[@data-ref='#statistics']")); // had //a at the end
+
+        	page.click();
+        }
+//        driver.close();
 	}
 }
